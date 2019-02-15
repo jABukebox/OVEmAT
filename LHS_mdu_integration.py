d1093a2592b24752d711ad353ba245e10bba9d2f
Jaron Bucka <jar.buck@gmx.de>
2019-02-15 15:30:58 +0100

    changed varFinal - added PHEV loop

--- LHS_mdu_integration.py
+++ LHS_mdu_integration.py
@@ -27,7 +27,6 @@ def lhs_dimension():                  # Dynamic dimension of LHS - depending on 
     dim = (len(all_range))  # Length of Variable list # TODO: len(range_variablen)
     return dim
 
-
 def LatinHype(dimension, n):  # n = number of samples
     points = pyDOE.lhs(dimension, samples=n)
     return points  # output.type = array
@@ -49,27 +48,49 @@ def varFinal():
     propType = ['BEV','FCEV','PHEV','ICEV']
     var_all = []
     for vehicle in range(len(propType)):                                         # Durchlauf jedes propTypes
-        gV = getVariables(class_sel, vehicle)                                      # holt die
-        lhs_items = 0
+        gV = getVariables(class_sel, vehicle)                                      # holt die Values aus getVariables
+        lhs_items = 0                                                            # TODO: muss bei PHEV evtl verändert werden
         var_array = []
+        if vehicle ==2:                 # PHEV #
+            for r in range(n):
+                var_list = []
+                m = 0
+                t = 1
+                dual = 0                                  # Changes getVariable from BEV to ICEV
+                while dual < 4:                           # 0 ^= BEV
+                    gV = getVariables(class_sel, dual)
+                    for k in range(dimension):  # alle Range-Werte mit reihe des LHS multiplizieren
+                        var_max = gV.iloc[m][t]  # bestimmung des max Wertes der eingegebenen Range
+                        t -= 1
+                        var_min = gV.iloc[m][t]  # bestimmung des min Wertes der eingegebenen Range
+                        t += 1
+                        var = (p.item(lhs_items) * (
+                                    var_max - var_min)) + var_min  # Verrechnung der Variablen mit LHS Ergebnissen in var
+                        var_list.append(var)  # Anhängen der Parameter an liste
+                        lhs_items += 1
+                        m += 1
+                    dual += 3           # Erhöhung um 3 (3 ^= ICEV)
+                var_array.append(var_list)
+            var_array = np.around(var_array, decimals = 4)
 
-        for r in range(n):                                                       # Anzahl der LHS Durchläufe
-            var_list = []                                                        # initiieren var_list: hier sollen pro 'n' alle verrechneten parameter in eine Liste gespeichert werden
-            m = 0               # row / zeile
-            t = 1               # column / spalte
-            for k in range(dimension):                                           # alle Range-Werte mit reihe des LHS multiplizieren
-                var_max = gV.iloc[m][t]                                          # bestimmung des max Wertes der eingegebenen Range
-                t -= 1
-                var_min = gV.iloc[m][t]                                          # bestimmung des min Wertes der eingegebenen Range
-                t += 1
-                var = (p.item(lhs_items) * (var_max - var_min)) + var_min        # Verrechnung der Variablen mit LHS Ergebnissen in var
-                var_list.append(var)                                             # Anhängen der Parameter an liste
-                lhs_items += 1
-                m += 1
-            var_array.append(var_list)                                           # Var_list gets appended to var_array
-        var_array = np.around(var_array, decimals=4)                             # round numbers
-    var_all.append(var_array)                                                    # Abspeicherung aller verrechneten
-    print(var_all)                                                               # propType Variablen
+        else:
+            for r in range(n):                                                       # Anzahl der LHS Durchläufe
+                var_list = []                                                        # initiieren var_list: hier sollen pro 'n' alle verrechneten parameter in eine Liste gespeichert werden
+                m = 0               # row / zeile
+                t = 1               # column / spalte
+                for k in range(dimension):                                           # alle Range-Werte mit reihe des LHS multiplizieren
+                    var_max = gV.iloc[m][t]                                          # bestimmung des max Wertes der eingegebenen Range
+                    t -= 1
+                    var_min = gV.iloc[m][t]                                          # bestimmung des min Wertes der eingegebenen Range
+                    t += 1
+                    var = (p.item(lhs_items) * (var_max - var_min)) + var_min        # Verrechnung der Variablen mit LHS Ergebnissen in var
+                    var_list.append(var)                                             # Anhängen der Parameter an liste
+                    lhs_items += 1
+                    m += 1
+                var_array.append(var_list)                                           # Var_list gets appended to var_array
+            var_array = np.around(var_array, decimals=4)                             # round numbers
+        var_all.append(var_array)                                                    # Abspeicherung aller verrechneten
+        print(var_all)                                                               # propType Variablen
     return var_all
 
 
@@ -82,6 +103,7 @@ def getVariables(class_sel, vehicle):
                 ['C3_batt', 'C5_empty', 'E_elGer', 'cd_empty', 'E_elCh', 'L', 'D', 'r', 'C_fuelEl', 'C_batt',
                  'C_fcEmpty'], axis='rows')
             range_vals = pd.concat([cc_bev, cg_bev])  # ZUSAMMENFÜHREN NACH GLEICHEN COLUMNS TODO: gin.default_general() vor varFinal
+
 
         elif vehicle == 1: # FCEV
             cc_fcev = gin.changed_compact().reindex(['FE_h2', 'E_battEmpty', 'P_batt', 'P_fc'], axis='rows')
@@ -96,6 +118,7 @@ def getVariables(class_sel, vehicle):
                  'E_batt', 'L', 'D', 'C_fuelEl', 'r', 'C_batt', 'S_renSmall'], axis='rows')
 
         elif vehicle == 3:  # ICEV
+
 
 
         else:
@@ -140,8 +163,6 @@ def getVariables(class_sel, vehicle):
         vehClassSel()                               # Erneute Eingabe der Fahrzeugklasse
 
     return range_vals
-
-
 
 
 #################################################################################
@@ -207,19 +228,19 @@ class LCE(Vehicles):
                 print("An error has occured! Please try again!")
         elif vehicle == 2:                                          # PHEV
             try:
-                if class_sel == 1:
+                if class_sel == 1:              # Compact
                     C3 = # Hier müssen werte aus LHS verrechnung rein
                     c_v = gin.default_compact()  # bv = bev_vals
 
-                elif class_sel == 2:
+                elif class_sel == 2:            # SUV
 
 
-                elif class_sel == 3:
-                    ll
+                elif class_sel == 3:            # LDV
+                    C3 =
                 cs = 1 - self.cd/100
-                E_fc_cs = C3 *self.FE * self.E_elGer + self.C5 * self.FE # Hier werte für ICEV
-                E_fc_cd = # Hier werte für BEV
-                E_fc = (E_fc_cs * cs + E_fc_cd * cd)/100
+                E_fc_cs = self.C3 *self.FE * self.E_elGer + self.C5 * self.FE # Hier werte für ICEV
+                E_fc_cd = ... # Hier werte für BEV
+                E_fc = (E_fc_cs * cs + E_fc_cd * self.cd)/100
                 return E_fc
             except:
                 print("An error has occured! Please try again!")
@@ -263,42 +284,42 @@ class TCO(Vehicles):
 
 
 
-# =============================================================================
-# Data Input & Print
-# =============================================================================
-# print("Please type in the fuel_cycle values: \n")
-print('\n\n\n')  # toyota ...
-
-# fuel_val = (0,0,13.814,0,0,0,28.06,489)
-# FCEV1 = fuel_cycle(fuel_val)
-
-
-
-FCEV1 = FuelCycle(0, 0, 13.814, 0, 0, 0, 28.06, 489)
-# print(FCEV1)
-FCEV2 = VehicleCycle(335.25, 1.124, 1.074, 2.41, 2.43, 1.25, 5.01, 6.22, 0, 0, 0, 5, 56.48, 40.89, 24.3, 489, 40, 0, 90,
-                     1850)
-# print(FCEV2)
-FCEV3 = TCO(0, 20000, 10000, 15000, 0, 3000, 0, 0, 14, 20000, 1.5, 7.4, 66, 150, 0, 0, 0,
-            0)  # hier darf 13. eintrag nicht 0 sein!! fehler ABFANGEN!
-# print(FCEV3)
-FCEV4 = LCE(VehicleCycle.calcVehicleCycle(FCEV2), FuelCycle.calcFuelCycle(FCEV1), 15, 10000)
-# FCEV4 = LCE(self.E_vc, self.E_fc, 15, 10000)
-# print(FCEV4)
-
-# emp_2 = Employee('Test', 'User', '12315')
-
-# =============================================================================
-# Output in Terminal
-# =============================================================================
-
-print("Fuel Cycle:\t\t\t"         + str(FuelCycle.calcFuelCycle(FCEV1))       + ' ' + "gCO_2eq / km")
-print("\nVehicle Cycle:\t\t"    + str(VehicleCycle.calcVehicleCycle(FCEV2)) + ' ' + "gCO_2eq")
-print("\n\nTotal Cost per km:\t"  + str(TCO.calcTCO(FCEV3))                   + ' ' + "€ / km")
-print("\nGesamte LCE:\t\t"    + str(LCE.calcLCE(FCEV4))                   + ' ' + "gCO_2 / km")
-
-
-#################################################################################
+# # =============================================================================
+# # Data Input & Print
+# # =============================================================================
+# # print("Please type in the fuel_cycle values: \n")
+# print('\n\n\n')  # toyota ...
+#
+# # fuel_val = (0,0,13.814,0,0,0,28.06,489)
+# # FCEV1 = fuel_cycle(fuel_val)
+#
+#
+#
+# FCEV1 = FuelCycle(0, 0, 13.814, 0, 0, 0, 28.06, 489)
+# # print(FCEV1)
+# FCEV2 = VehicleCycle(335.25, 1.124, 1.074, 2.41, 2.43, 1.25, 5.01, 6.22, 0, 0, 0, 5, 56.48, 40.89, 24.3, 489, 40, 0, 90,
+#                      1850)
+# # print(FCEV2)
+# FCEV3 = TCO(0, 20000, 10000, 15000, 0, 3000, 0, 0, 14, 20000, 1.5, 7.4, 66, 150, 0, 0, 0,
+#             0)  # hier darf 13. eintrag nicht 0 sein!! fehler ABFANGEN!
+# # print(FCEV3)
+# FCEV4 = LCE(VehicleCycle.calcVehicleCycle(FCEV2), FuelCycle.calcFuelCycle(FCEV1), 15, 10000)
+# # FCEV4 = LCE(self.E_vc, self.E_fc, 15, 10000)
+# # print(FCEV4)
+#
+# # emp_2 = Employee('Test', 'User', '12315')
+#
+# # =============================================================================
+# # Output in Terminal
+# # =============================================================================
+#
+# print("Fuel Cycle:\t\t\t"         + str(FuelCycle.calcFuelCycle(FCEV1))       + ' ' + "gCO_2eq / km")
+# print("\nVehicle Cycle:\t\t"    + str(VehicleCycle.calcVehicleCycle(FCEV2)) + ' ' + "gCO_2eq")
+# print("\n\nTotal Cost per km:\t"  + str(TCO.calcTCO(FCEV3))                   + ' ' + "€ / km")
+# print("\nGesamte LCE:\t\t"    + str(LCE.calcLCE(FCEV4))                   + ' ' + "gCO_2 / km")
+#
+#
+# #################################################################################
 
 
 
@@ -306,7 +327,9 @@ print("\nGesamte LCE:\t\t"    + str(LCE.calcLCE(FCEV4))                   + ' ' 
 # Berechnung des results mit varFinal variablen
 # =============================================================================
 def resultCalc():
-
+    propType = ['BEV','FCEV','PHEV','ICEV']
+    for vehicle in range(len(propType)):
+        pass
     # Put all values together
     X_vals = list(gin.x_vals().loc['compact(bev)']) # get X_vals Todo: schleife für compact, suv etc ->
 
@@ -364,7 +387,7 @@ class PlotClass():
             self.plotting()
 
     def plotting(self):
-        self.i = 0
+        i = 0
 
         # create the view
         self.view = pg.PlotWidget()
@@ -380,7 +403,7 @@ class PlotClass():
         self.view.addItem(self.plot)
 
         # Convert data array into a list of dictionaries with the x,y-coordinates
-        self.pos = [{'pos': res[self.i, :]} for self.i in range(n)]
+        self.pos = [{'pos': res[i, :]} for i in range(n)]
         self.now = pg.ptime.time()
         self.plot.setData(self.pos)
 
@@ -390,18 +413,14 @@ class PlotClass():
 if __name__ == '__main__':
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    n = 100  # Number of repeats (test it!)
+    n = 10  # Number of repeats (test it!)
 
 
 
     # Call Functions
     gen_def = gin.changed_general()  # Einlesen der general defaults
 
-    if vehicle == 2:        #PHEV
-        all_range = pd.concat([gin.cc_phev, gin.cg_phev])  # für dimension length
-    else:
-        all_range = pd.concat([gin.cc_bev, gin.cg_bev]) #für dimension length
-
+    all_range = pd.concat([gin.cc_bev, gin.cg_bev]) #für dimension length
     dimension = lhs_dimension()  # Dimension, bzw. Zahl der Variablen
 
     class_sel = vehClassSel()
