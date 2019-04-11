8e72310e459a9f98e630a52aaa2ea85f27c58348
Alexander Wanitschke <alexander.wanitschke@gmail.com>
2019-04-11 12:04:53 +0200

    new values

--- src/main.py
+++ src/main.py
@@ -742,7 +742,7 @@ if __name__ == '__main__':
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     # Number of repeats
-    n = 200
+    n = 300
 
     # Call all function in run function
     execute = run(n)
